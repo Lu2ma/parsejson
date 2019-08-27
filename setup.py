c1bb8e3ff190dab5d7d219022f102ae6bd25a249
lu dr <ludr@GDC-Mac-Host.local>
2019-08-27 16:05:25 +0800

    Modify setup.py

--- setup.py
+++ setup.py
@@ -37,6 +37,5 @@ setup(
 		'License :: OSI Approved :: MIT License',
 		'Programming Language :: Python',
 	        'Programming Language :: Python :: 3.6'
-		# 'Programming Language :: Python :: 3.1',
 	],
 )
